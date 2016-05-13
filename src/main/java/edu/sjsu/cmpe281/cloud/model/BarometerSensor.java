586642ddc0479a90665672ebb6861a792fae9060
Nakul Sharma <nakulsharma06@gmail.com>
2016-05-13 16:26:20 -0700

    collection name changed

--- src/main/java/edu/sjsu/cmpe281/cloud/model/BarometerSensor.java
+++ src/main/java/edu/sjsu/cmpe281/cloud/model/BarometerSensor.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
  */
 
 
-@Document(collection = "VirtualSensor")
+@Document(collection = "BarometerData")
 public class BarometerSensor {
 
     @Id
