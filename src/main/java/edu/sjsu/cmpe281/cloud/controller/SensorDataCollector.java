5f295dd91ba3b8ff3ccad8642bfb6fdb4a9c238a
Savanigit config --global user.name S <savani.vishnoi@gmail.com>
2016-05-17 01:33:25 -0700

    API for pricing

--- src/main/java/edu/sjsu/cmpe281/cloud/controller/SensorDataCollector.java
+++ src/main/java/edu/sjsu/cmpe281/cloud/controller/SensorDataCollector.java
@@ -1,7 +1,13 @@
 package edu.sjsu.cmpe281.cloud.controller;
 
+import edu.sjsu.cmpe281.cloud.dto.VirtualSensorCrudImpl;
 import edu.sjsu.cmpe281.cloud.model.BarometerSensor;
+import edu.sjsu.cmpe281.cloud.model.VirtualSensor;
+import edu.sjsu.cmpe281.cloud.service.ICencoosConnect;
 import edu.sjsu.cmpe281.cloud.service.IMongoService;
+import edu.sjsu.cmpe281.cloud.service.IVirtualSensor;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +16,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -26,8 +36,21 @@ public class SensorDataCollector {
 
     @Autowired
     IMongoService mongoService;
+    @Autowired
+    IVirtualSensor virtualSensor;
+    @Autowired
+    ICencoosConnect cencoosConnect;
 
 
+
+    @RequestMapping(method = RequestMethod.GET, value = "/update/backend")
+    public ResponseEntity getCencoosData(@RequestParam(value = "timestamp", required = true) String timestamp) {
+        boolean fetchStatus = cencoosConnect.fetchData(timestamp);
+        if (fetchStatus)
+            return new ResponseEntity<>(HttpStatus.OK);
+        else
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
     /**
      * Fetch data from mongo db database
      *
@@ -79,5 +102,40 @@ public class SensorDataCollector {
             logger.error("Exception: " + e.getMessage());
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{user_id}/pricing")
+    public ResponseEntity fetchPricing(@PathVariable(value = "user_id") String userId){
+         JSONArray responseObject = new JSONArray();
+        JSONObject jsonObject =  new JSONObject();
+        VirtualSensorCrudImpl vs = new VirtualSensorCrudImpl();
+        List<VirtualSensor> virtualSensorsList = virtualSensor.getVirtualSensorListByUserId(userId);
+        if(virtualSensorsList.size() == 0){ return new ResponseEntity("No sensors.",HttpStatus.BAD_REQUEST); }
+        Double price = 0.0;
+        String timeCreated = null;
+        String timeUpdated = null;
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE, MMM dd, yyyy HH:mm:ss a");
+        long diff;
+        try {
+        for(int i = 0 ; i < virtualSensorsList.size(); i++){
+
+            timeCreated = ( virtualSensorsList.get(i).getTimecreated());
+            timeUpdated = (virtualSensorsList.get(i).getTimeupdated());
+           // diff = timeUpdated - timeCreated;
+            Date date1 = formatter.parse(timeCreated);
+            Date date2 = formatter.parse(timeUpdated);
+            diff = date2.getTime() - date1.getTime();
+                jsonObject.put("sensorid", virtualSensorsList.get(i).getSensorid());
+                jsonObject.put("totaltime", diff);
+            }
+            responseObject.put(jsonObject);
+            //price =  price + 0.01 * diff;
+      }
+        catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return  new ResponseEntity<>(responseObject.toString(), HttpStatus.OK);
     }
 }
